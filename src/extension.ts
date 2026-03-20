caf6d4fa5e7bc0c2160c7e41b114dd77309c7744
Oliver Tušla <oliver.tusla@seznam.cz>
2026-03-20 12:14:55 +0100

    basic logs

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,18 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+	console.log("Activating..");
+
 	const disposable = vscode.commands.registerCommand("mlsem.helloWorld", () => {
 		vscode.window.showInformationMessage("Hello from MLsem!");
 	});
 
 	context.subscriptions.push(disposable);
+
+	console.log("Activated.");
 }
 
-export function deactivate() {}
+export function deactivate() {
+	console.log("Deactivating..");
+	console.log("Deactivated.");
+}
